d0093b7cd8de3c404890436a221e4998220e48bd
Mathieu <78476132+MathieuTOUDIC@users.noreply.github.com>
2024-03-08 08:46:29 +0100

    Add files via upload
    the first code of the alimentation value recover

--- alim.py
+++ alim.py
@@ -1,42 +1,25 @@
-import matplotlib.pyplot as plt
-import time
 import requests
 from bs4 import BeautifulSoup
+import time
 
-# fonction pour récupérer la valeur à partir de la page web
-def get_value():
-    url = "http://example.com/page.html"
-    response = requests.get(url)
-    #Extrait la valeur à partir du contenu de la page web
-    soup = BeautifulSoup(response.text, 'html.parser')
-    
-    #Trouver l'input avec l'attribut id "actcur"
-    input_field = soup.find('input', {'id':'actcur'})
-    if input_field is not None:
-        value = input_field["value"]
-    else:
-        value = None
-
-    print(value)
-    return float(value.replace(" A", ""))
-
-# créer un graphique ligne vide
-fig, ax = plt.subplots()
-line, = ax.plot([], [])
-ax.set_ylim(0, 1)
-
-# boucle infinie pour mettre à jour le graphique en temps réel
+url = 'http://192.168.0.2/Home.cgi'
+i=0
 while True:
-    # récupérer la nouvelle valeur à partir de la page web
-    value = get_value()
+    response = requests.get(url)
+    if response.status_code == 200:
+        #Extrait la valeur à partir du contenu de la page web
+        soup = BeautifulSoup(response.text, 'html.parser')
 
-    # mettre à jour les données du graphique
-    line.set_data([value], [value])
-    ax.set_xlim(value-0.01, value+0.01)
+        #Trouver l'input avec l'attribut id "actcur"
+        input_element = soup.find('input', {'id':'actcur'})
 
-    # redessiner le graphique
-    fig.canvas.draw()
-    fig.canvas.flush_events()
+        #Extrait la valeur de l'attribut "value"
+        value = input_element['value']
 
-    # attendre une seconde avant la prochaine mise à jour
+        print(i,value)
+        i+=1
+    else:
+        print(f"Erreur {response.status_code} lors de la récupération de la page wed de l'alimentation")
+
+    # Attend une seconde avant la prohaine requête 
     time.sleep(1)
